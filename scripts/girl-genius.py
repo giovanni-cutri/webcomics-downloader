6b8acacbb873446d95d735045db46a9d81b4b3c0
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2022-12-30 16:11:02 +0100

    Added support for girl-genius

---
+++ scripts/girl-genius.py
@@ -0,0 +1,32 @@
+def get_first_url():
+    return "https://www.girlgeniusonline.com/comic.php?date=20021104"
+
+
+def get_title(soup):
+    title = soup.select("#datestring")[0].getText()
+    return title
+
+
+def get_images_url(soup):
+
+    images = []
+
+    try:
+        elements = soup.select("IMG[ALT='Comic']")
+
+        for i in elements:
+            images.append(i.attrs["src"])
+    except IndexError:
+        pass
+
+    return images
+
+
+def get_next_page_url(soup):
+
+    try:
+        next_page_url = soup.select("a#topnext")[0].attrs["href"]
+    except IndexError:
+        next_page_url = []
+
+    return next_page_url
