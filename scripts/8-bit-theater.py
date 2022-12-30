3e3add01221122d0565038de2b0e90fe3a10aa6f
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2022-12-30 15:52:07 +0100

    Added support for 8-bit-theater

---
+++ scripts/8-bit-theater.py
@@ -0,0 +1,35 @@
+def get_first_url():
+    return "https://www.nuklearpower.com/2001/03/02/episode-001-were-going-where/"
+
+
+def get_title(soup):
+    try:
+        title = soup.select("div.navbar-title")[0].getText()
+    except IndexError:
+        title = "no-title"
+    return title
+
+
+def get_images_url(soup):
+
+    images = []
+
+    try:
+        elements = soup.select("#comic img")
+
+        for i in elements:
+            images.append(i.attrs["src"])
+    except IndexError:
+        pass
+
+    return images
+
+
+def get_next_page_url(soup):
+
+    try:
+        next_page_url = soup.select("a[rel='next']")[0].attrs["href"]
+    except IndexError:
+        next_page_url = []
+
+    return next_page_url
