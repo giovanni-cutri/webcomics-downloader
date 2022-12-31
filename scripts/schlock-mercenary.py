c8e93e1873a53cf17bf620a4ca117eb8e9de997e
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2022-12-31 10:41:04 +0100

    Added support for schlock-mercenary

---
+++ scripts/schlock-mercenary.py
@@ -0,0 +1,36 @@
+def get_first_url():
+    return "https://www.schlockmercenary.com/2000-06-12"
+
+
+def get_title(soup):
+    try:
+        title = soup.select(".strip-date")[0].getText().strip()
+    except IndexError:
+        title = "no-title"
+        
+    return title
+
+
+def get_images_url(soup):
+
+    images = []
+
+    try:
+        elements = soup.select("meta[property='og:image']")
+
+        for i in elements:
+            images.append(i.attrs["content"].split("?")[0])
+    except IndexError:
+        pass
+
+    return images
+
+
+def get_next_page_url(soup):
+
+    try:
+        next_page_url = "https://www.schlockmercenary.com" + soup.select(".next-strip")[0].attrs["href"]
+    except IndexError:
+        next_page_url = []
+
+    return next_page_url
