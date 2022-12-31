ab2b66fa2c801adac4b08e41176b23c4d445c208
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2022-12-31 15:55:15 +0100

    Create bonkers-world.py
    Added support for Bonkers World

---
+++ scripts/bonkers-world.py
@@ -0,0 +1,37 @@
+def get_first_url():
+    return "https://bonkersworld.net/hotmail-doesnt-work-with-linux-firefox-2-0"
+
+
+def get_title(soup):
+    try:
+        title = soup.select("title")[0].getText()
+    except IndexError:
+        title = "no-title"
+    return title
+
+
+def get_images_url(soup):
+
+    images = []
+
+    try:
+        elements = soup.select("p img")
+        for i in elements:
+            images.append("https://bonkersworld.net/" + i.attrs["src"])
+    except IndexError:
+        pass
+
+    return images
+
+
+def get_next_page_url(soup):
+
+    try:
+        next_page_url = "https://bonkersworld.net/" + soup.select("#nav")[0].attrs["data-nxt"]
+    except IndexError:
+        next_page_url = []
+        
+    if next_page_url == "https://bonkersworld.net/":
+        next_page_url = []
+
+    return next_page_url
