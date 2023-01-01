41ff4a1629294ef8079cda83601b7b58a1e390ac
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2023-01-01 16:04:09 +0100

    Create experience-boost.py
    Added support for "Experience Boost"

---
+++ scripts/experience-boost.py
@@ -0,0 +1,34 @@
+def get_first_url():
+    return "https://www.xpboostcomic.com/comic/first-steps/"
+
+
+def get_title(soup):
+    try:
+        title = soup.select("meta[property='og:title']")[0].attrs["content"].split(" | ")[0]
+    except IndexError:
+        title = "no-title"
+    return title
+
+
+def get_images_url(soup):
+
+    images = []
+
+    try:
+        elements = soup.select("meta[property='og:image']")
+        for i in elements:
+            images.append(i.attrs["content"].split("?")[0])
+    except IndexError:
+        pass
+
+    return images
+
+
+def get_next_page_url(soup):
+
+    try:
+        next_page_url = soup.select(".comic-nav-next")[0].attrs["href"]
+    except IndexError:
+        next_page_url = []
+
+    return next_page_url
