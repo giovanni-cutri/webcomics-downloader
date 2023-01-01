8c2272eb7526c8a0ae32642b36491e1dc0286cb3
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2023-01-01 16:10:42 +0100

    Create full-frontal-nerdity.py
    Added support for "Full Frontal Nerdity"

---
+++ scripts/full-frontal-nerdity.py
@@ -0,0 +1,34 @@
+def get_first_url():
+    return "https://ffn.nodwick.com/?p=6"
+
+
+def get_title(soup):
+    try:
+        title = soup.select("meta[property='og:title']")[0].attrs["content"]
+    except IndexError:
+        title = "no-title"
+    return title
+
+
+def get_images_url(soup):
+
+    images = []
+
+    try:
+        elements = soup.select(".comicpane img")
+        for i in elements:
+            images.append(i.attrs["src"])
+    except IndexError:
+        pass
+
+    return images
+
+
+def get_next_page_url(soup):
+
+    try:
+        next_page_url = soup.select("a[rel='next']")[0].attrs["href"]
+    except IndexError:
+        next_page_url = []
+
+    return next_page_url
