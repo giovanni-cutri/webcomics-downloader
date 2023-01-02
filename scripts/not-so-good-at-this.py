10684b22249a65a3b7feac211e073add27a156fe
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2023-01-02 06:35:05 +0100

    Create not-so-good-at-this.py
    Added support for "Not So Good at This"

---
+++ scripts/not-so-good-at-this.py
@@ -0,0 +1,39 @@
+def get_first_url():
+    return "https://www.notsogoodatthis.com/6-2/"
+
+
+def get_title(soup):
+    try:
+        title = soup.select("meta[property='og:description']")[0].attrs["content"]
+    except IndexError:
+        try:
+            title = soup.select("meta[property='og:title']")[0].attrs["content"].split(" - ")[0]
+        except IndexError:
+            title = "no-title"
+
+    return title
+
+
+
+def get_images_url(soup):
+
+    images = []
+
+    try:
+        elements = soup.select("meta[property='og:image']")
+        for i in elements:
+            images.append(i.attrs["content"])
+    except IndexError:
+        pass
+
+    return images
+
+
+def get_next_page_url(soup):
+
+    try:
+        next_page_url = soup.select("a[rel='next']")[0].attrs["href"]
+    except IndexError:
+        next_page_url = []
+
+    return next_page_url
