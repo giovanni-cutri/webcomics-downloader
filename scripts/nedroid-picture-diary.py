7f0fb75f9bce4ac27b4a7360ced83e48385e71a6
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2023-01-02 06:22:59 +0100

    Create nedroid-picture-diary.py
    Added support for "Nedroid Picture Diary"

---
+++ scripts/nedroid-picture-diary.py
@@ -0,0 +1,34 @@
+def get_first_url():
+    return "http://nedroid.com/2005/09/2210-whee/"
+
+
+def get_title(soup):
+    try:
+        title = soup.select("#comic img")[0].attrs["alt"]
+    except IndexError:
+        title = "no-title"
+    return title
+
+
+def get_images_url(soup):
+
+    images = []
+
+    try:
+        elements = soup.select("#comic img")
+        for i in elements:
+            images.append(i.attrs["src"])
+    except IndexError:
+        pass
+
+    return images
+
+
+def get_next_page_url(soup):
+
+    try:
+        next_page_url = soup.select("link[rel='next']")[0].attrs["href"]
+    except IndexError:
+        next_page_url = []
+        
+    return next_page_url
